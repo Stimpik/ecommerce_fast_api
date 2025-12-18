0cd29b92867326eadbb815a5b20509ebf1f6cad3
Stimpik <iderik@mail.ru>
2025-12-18 18:14:37 +0300

    Add auth seller

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
 from decimal import Decimal
+from typing import Optional
 
 
 class CategoryCreate(BaseModel):
@@ -33,7 +34,7 @@ class ProductCreate(BaseModel):
     name: str = Field(min_length=3, max_length=100,
                       description="Название товара (3-100 символов)")
     description: str | None = Field(None, max_length=500,
-                                       description="Описание товара (до 500 символов)")
+                                    description="Описание товара (до 500 символов)")
     price: Decimal = Field(gt=0, description="Цена товара (больше 0)", decimal_places=2)
     image_url: str | None = Field(None, max_length=200, description="URL изображения товара")
     stock: int = Field(ge=0, description="Количество товара на складе (0 или больше)")
@@ -54,4 +55,18 @@ class Product(BaseModel):
     category_id: int = Field(description="ID категории")
     is_active: bool = Field(description="Активность товара")
 
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    email: EmailStr = Field(description="Email пользователя")
+    password: str = Field(min_length=8, description="Пароль (минимум 8 символов)")
+    role: str = Field(default="buyer", pattern="^(buyer|seller)$", description="Роль: 'buyer' или 'seller'")
+
+
+class User(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    role: str
     model_config = ConfigDict(from_attributes=True)
